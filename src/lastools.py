5080abc4f62b468ae8fe6b5bcc828336d547a2a8
Afshin <afshinshah77@gmail.com>
2022-11-01 12:11:12 -0700

    df export to csv added

--- src/lastools.py
+++ src/lastools.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import laspy
-
+from pathlib import Path
 
 def las_to_df(las_file):
     data = []
@@ -10,8 +10,11 @@ def las_to_df(las_file):
     df = pd.DataFrame(np.array(data).T, columns=las_file.point_format.dimension_names)
     return df
 
+def _write_df_to_csv(df, name, address):
+    df.to_csv(Path(f'{address}')/f'{name}.csv')
 
 if __name__ == '__main__':
     las_file = laspy.read('../data/test.las')
     df = las_to_df(las_file)
     print(df)
+    _write_df_to_csv(df,'tt','../data/')
