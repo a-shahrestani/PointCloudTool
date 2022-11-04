3aaddd5744616cb0ac5d1e9e71e401afb316b64e
Afshin <afshinshah77@gmail.com>
2022-11-04 12:06:50 -0700

    Intensity reduction function added - classification standardization function in progress - base readme added

---
+++ README.md
@@ -0,0 +1,1 @@
+A set of functions for LAS/LAZ/PLY point cloud data manipulation. These functions are meant to turn into a command-line tool
--- src/lastools.py
+++ src/lastools.py
@@ -10,10 +10,10 @@ _standard_classes = {'never classified': [0], 'unclassified': [1], 'ground': [2]
                      'medium vegetation': [4], 'high vegetation': [5], 'building': [6], 'low point': [7],
                      'high point': [8], 'water': [9], 'rail': [10], 'road surface': [11], 'bridge deck': [12],
                      'wire-guard': [13], 'wire-conductor': [14], 'transmission tower': [15],
-                     'wire-structure connector': [16], 'reserved': range(17, 63)}
+                     'wire-structure connector': [16]}
 
 
-def las_to_df(las_file):
+def _las_to_df(las_file):
     data = []
     for column in list(las_file.point_format.dimension_names):
         data.append(np.array(las_file[column]))
@@ -33,8 +33,27 @@ def _laz_to_las_conversion(laz_file, address, name):
     laz_file.write(do_compress=False, destination=Path(f'{address}') / f'{name}.las')
 
 
-def _las_class_standardization(classes):
+def _json_class_mapping(file):
     pass
+
+
+def _las_class_standardization(df: pd.DataFrame, classes_file=None, classes_json=None,
+                               classes=None) -> pd.DataFrame:
+    return df
+
+
+def _df_intensity_reduction(df: pd.DataFrame, percentage=None, count=None) -> pd.DataFrame:
+    if percentage is not None:
+        remove_n = len(df) // 100 * percentage
+    elif count is not None:
+        remove_n = count
+    else:
+        raise Exception("Either count of the points or the percentage of dropped points should be set")
+    drop_indices = np.random.choice(df.index, remove_n, replace=False)
+    df_subset = df.drop(drop_indices)
+    return df_subset
+
+
 def _df_to_las_conversion(df, address, name, las_version=(1, 4), las_format=7, data_columns=[]):
     header = laspy.header.LasHeader(version=Version(las_version[0], las_version[1]),
                                     point_format=PointFormat(las_format))
@@ -50,7 +69,7 @@ def _df_to_las_conversion(df, address, name, las_version=(1, 4), las_format=7, d
 
 if __name__ == '__main__':
     las_file = laspy.read('../data/ttlaz.laz')
-    df = las_to_df(las_file)
+    df = _las_to_df(las_file)
     # print(df)
     # _write_df_to_csv(df, '../data/', 'ttlaz')
     # _laz_to_las_conversion(las_file, '../data/', 'ttlas')
